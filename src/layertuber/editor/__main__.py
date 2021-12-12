86b1522e583d0f80908e87587b5a4a5305b36ec5
colons <colons@colons.co>
2021-12-12 00:34:03 +0000

    Populate a GTK tree view with the layers of a rig.

--- src/layertuber/editor/__main__.py
+++ src/layertuber/editor/__main__.py
@@ -1,5 +1,14 @@
+import argparse
+
 from .window import main
 
 
+def _parse_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(add_help=False)
+    parser.add_argument('rig_path')
+    return parser.parse_args()
+
+
 if __name__ == '__main__':
-    main()
+    args = _parse_args()
+    main(rig_path=args.rig_path)
--- src/layertuber/editor/window.py
+++ src/layertuber/editor/window.py
@@ -1,9 +1,26 @@
 import gi; gi.require_version('Gtk', '3.0')  # noqa
 from gi.repository import Gtk
 
+from ..rig.rig import Rig
 
-def main() -> None:
-    window = Gtk.Window(title='layertuber editor')
-    window.show()
+
+class EditorWindow(Gtk.Window):
+    def __init__(self, rig: Rig) -> None:
+        self.rig = Rig
+        super().__init__(title='layertuber editor')
+        store = Gtk.TreeStore(str)
+        tree = Gtk.TreeView(model=store)
+
+        for layer in rig.layers:
+            store.append(None, [layer.name])
+
+        column = Gtk.TreeViewColumn('layer', Gtk.CellRendererText(), text=0)
+        tree.append_column(column)
+        self.add(tree)
+
+
+def main(rig_path: str) -> None:
+    window = EditorWindow(Rig(rig_path, (800, 600)))
+    window.show_all()
     window.connect('destroy', Gtk.main_quit)
     Gtk.main()
