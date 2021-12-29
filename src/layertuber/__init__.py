ffb66477f38b5412c5ec87cdeb03998a4a8b514e
colons <colons@colons.co>
2021-12-29 21:25:42 +0000

    Make the output of `layertuber --help` much more useful.

--- src/layertuber/__init__.py
+++ src/layertuber/__init__.py
@@ -1,6 +1,6 @@
 import argparse
 import logging
-import os
+import os; os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = 'true'  # noqa
 from queue import Queue
 from threading import Thread
 from typing import Optional
@@ -19,10 +19,12 @@ logging.basicConfig(
 
 
 def _parse_args() -> argparse.Namespace:
-    parser = argparse.ArgumentParser(add_help=False)
-    parser.add_argument('-c', '--camera', type=int, default=0)
-    parser.add_argument('-w', '--width', type=int, default=800)
-    parser.add_argument('-h', '--height', type=int, default=600)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-c', '--camera', type=int, default=0, help=(
+        'The index of the camera to use. If your computer has only one webcam, you can leave this at its default 0.'
+    ))
+    parser.add_argument('-x', '--output-width', type=int, default=800)
+    parser.add_argument('-y', '--output-height', type=int, default=600)
     parser.add_argument('rig_path')
     return parser.parse_args()
 
