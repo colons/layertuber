4fc4b6a2edd92be281a6cd21ff7aff3160251f05
colons <colons@colons.co>
2022-08-28 19:12:46 +0100

    Run the python reporter from the rust process.

--- src/py.rs
+++ src/py.rs
@@ -1,7 +1,12 @@
 use std::thread;
 use std::time::Duration;
+use subprocess::{Popen, PopenConfig};
 
 pub fn run_tracker() {
+    let p = Popen::create(&["layertuber"], PopenConfig {
+        // stdout: Redirection::Pipe,
+        ..Default::default()
+    });
     loop {
         thread::sleep(Duration::from_secs(1));
         println!("from py host");
