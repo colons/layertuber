8b45e3ba818d556e44c9142fafa94de66e3bd395
colons <colons@colons.co>
2022-08-28 21:43:24 +0100

    Attempt to write and run the static tracker binary.
    I think I should be using a cache dir instead of a tmpfile.

--- src/py.rs
+++ src/py.rs
@@ -1,21 +1,57 @@
 use std::thread;
 use std::time::Duration;
+use std::fs;
+use std::mem::drop;
+use std::os::unix::fs::PermissionsExt;
+use std::io::Write;
 use subprocess::{ExitStatus, Popen, PopenConfig};
+use tempfile::NamedTempFile;
 
 const TRACKER_BIN: &'static [u8] = include_bytes!("py/dist/layertuber");
 
 pub fn run_tracker() {
-    let mut p = Popen::create(&["layertuber"], PopenConfig {
+    let mut tracker_bin = NamedTempFile::new().expect("could not make temporary file for tracker");
+    tracker_bin.write(TRACKER_BIN).expect("failed to write tracker binary");
+    tracker_bin.flush().expect("failed to flush tracker");
+
+    let (file, path) = tracker_bin.keep().expect("failed to persist tracker");
+
+    let metadata = file.metadata().expect("could not read metadata");
+    let mut permissions = metadata.permissions();
+    permissions.set_mode(0o500);
+    fs::set_permissions(&path, permissions).expect("could not make tracker executable");
+
+    drop(file);
+
+    let cleanup = || {
+        match fs::remove_file(&path) {
+            Ok(_) => eprintln!("deleted tracker"),
+            Err(e) => eprintln!("tracker deletion failed: {}", e),
+        };
+    };
+
+    let mut p = match Popen::create(&[&path], PopenConfig {
         // stdout: redirection::pipe,
         ..Default::default()
-    }).expect("failed to start tracker");
+    }) {
+        Ok(p) => p,
+        Err(e) => {
+            cleanup();
+            panic!("failed to start tracker: {}", e)
+        }
+    };
 
     loop {
         match p.poll() {
-            Some(ExitStatus::Exited(s)) => panic!("tracker died with exit code {}", s),
-            Some(ExitStatus::Signaled(s)) => panic!("tracker died with signal {}", s),
-            Some(ExitStatus::Other(s)) => panic!("tracker died for some reason: {}", s),
-            Some(ExitStatus::Undetermined) => panic!("tracker died for some reason"),
+            Some(e) => {
+                cleanup();
+                match e {
+                    ExitStatus::Exited(s) => panic!("tracker died with exit code {}", s),
+                    ExitStatus::Signaled(s) => panic!("tracker died with signal {}", s),
+                    ExitStatus::Other(s) => panic!("tracker died for some reason: {}", s),
+                    ExitStatus::Undetermined => panic!("tracker died for some reason"),
+                }
+            }
             None => (),
         };
         thread::sleep(Duration::from_secs(1));
