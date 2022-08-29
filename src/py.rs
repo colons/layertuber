0d55aaf7a1c124ad95dc72da75d03ce33bcaba30
colons <colons@colons.co>
2022-08-29 09:26:34 +0100

    Use the cache dir instead of /tmp to store the tracker binary.

--- src/py.rs
+++ src/py.rs
@@ -3,34 +3,40 @@ use std::time::Duration;
 use std::fs;
 use std::mem::drop;
 use std::os::unix::fs::PermissionsExt;
+use std::fs::File;
 use std::io::Write;
+use dirs::cache_dir;
 use subprocess::{ExitStatus, Popen, PopenConfig};
-use tempfile::NamedTempFile;
 
 const TRACKER_BIN: &'static [u8] = include_bytes!("py/dist/layertuber");
 
 pub fn run_tracker() {
-    let mut tracker_bin = NamedTempFile::new().expect("could not make temporary file for tracker");
+    let cd = match cache_dir() {
+        Some(p) => p,
+        None => panic!("what's a cache directory"),
+    };
+    let tracker_bin_path = cd.join("layertuber-tracker");
+
+    let mut tracker_bin = File::create(&tracker_bin_path).expect("failed to create tracker binary");
+
     tracker_bin.write(TRACKER_BIN).expect("failed to write tracker binary");
     tracker_bin.flush().expect("failed to flush tracker");
 
-    let (file, path) = tracker_bin.keep().expect("failed to persist tracker");
-
-    let metadata = file.metadata().expect("could not read metadata");
+    let metadata = tracker_bin.metadata().expect("could not read metadata");
     let mut permissions = metadata.permissions();
     permissions.set_mode(0o500);
-    fs::set_permissions(&path, permissions).expect("could not make tracker executable");
+    fs::set_permissions(&tracker_bin_path, permissions).expect("could not make tracker executable");
 
-    drop(file);
+    drop(tracker_bin);
 
     let cleanup = || {
-        match fs::remove_file(&path) {
+        match fs::remove_file(&tracker_bin_path) {
             Ok(_) => eprintln!("deleted tracker"),
             Err(e) => eprintln!("tracker deletion failed: {}", e),
         };
     };
 
-    let mut p = match Popen::create(&[&path], PopenConfig {
+    let mut p = match Popen::create(&[&tracker_bin_path], PopenConfig {
         // stdout: redirection::pipe,
         ..Default::default()
     }) {
