b95c1c7f5648580da0a13290b5512665389326f3
colons <colons@colons.co>
2022-08-30 20:53:26 +0100

    Format.

--- src/puppet/rig.rs
+++ src/puppet/rig.rs
@@ -1,14 +1,15 @@
-use std::path::Path;
 use std::io;
+use std::path::Path;
 
 pub struct Rig {
-    pub layers: Vec<u8>,  // XXX some kind of texture from three-js?
+    pub layers: Vec<u8>, // XXX some kind of texture from three-js?
 }
 
 impl Rig {
     pub fn open(ora_path: &Path) -> io::Result<Rig> {
         println!("{}", ora_path.display());
-        Ok(Rig{
+
+        Ok(Rig {
             layers: Vec::from([]),
         })
     }
