2fe4a5d1d6a5ca697a1272f3e663e44c41fe9ea6
colons <colons@colons.co>
2022-08-30 18:02:14 +0100

    Make puppet a folder.

--- src/puppet/mod.rs
+++ src/puppet/mod.rs
@@ -1,11 +1,8 @@
 use crate::tracker::TrackingReport;
 use std::sync::mpsc::Receiver;
-use std::thread;
-use std::time::Duration;
 
 pub fn run_puppet(rx: Receiver<TrackingReport>) {
     loop {
-        thread::sleep(Duration::from_secs(1));
         dbg!(rx.recv().unwrap());
     }
 }
